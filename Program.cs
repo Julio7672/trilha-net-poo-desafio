3897bcb525832ffa0a27b3dc8ea16c4c1def2815
julio alexandre duarte da silva <113787478+Julio7672@users.noreply.github.com>
2022-10-18 19:50:10 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿using DesafioPOO.Models;
 
+
+
+
+
+
+
+        Console.WriteLine("Smartphone Iphone:");
+        Smartphone iphone = new Iphone(numero: "813490323", modelo: "iphone t45", imei: "111112222",   memoria: 128 );
+        iphone.InstalarAplicativo(nomeApp: "YouTube");
+        iphone.ReceberLigacao();
+        iphone.Ligar();
+    
+
+          Console.WriteLine("\n");
+    
+         
+        Console.WriteLine("Smartphoone Nokia:");
+        Smartphone nokia = new Nokia(numero: "810290323", modelo: "Nokia k55", imei: "222222222",   memoria: 64 );
+        nokia.InstalarAplicativo(nomeApp: "TikTok");
+        nokia.ReceberLigacao();
+        nokia.Ligar();
+    
+
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
